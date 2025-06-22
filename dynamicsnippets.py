f998e56226beb73a8f11fbb00af8edf6a21607c2
Ethan Eldridge <ejayeldridge@gmail.com>
2025-06-21 22:41:55 -0400

    make section snippet dynamic

--- dynamicsnippets.py
+++ dynamicsnippets.py
@@ -17,9 +17,23 @@ class DynamicSnippetsViewEventListener(sublime_plugin.ViewEventListener):
 		self.footnote_ref_snippet = """<sup id="footnote-{0}-ref"><a href="#footnote-{0}">{0}</a></sup>$0"""
 		self.number_of_footnotes_in_file = 0
 
+		self.new_section_snippet= inspect.cleandoc("""
+		<section id="section-{0}">
+		    <h2>$1 <a href="#toc">&#8617;</a></h2>
+		    <p>
+		        $0
+		    </p>
+		</section>
+		""")
+		self.number_of_sections_in_file = 0
+
 	def calculate_footnote_count(self):
 		footnotes_in_view = self.view.find_all("id=\"footnote-[0-9]+\"", sublime.FindFlags.WRAP)
 		self.number_of_footnotes_in_file = len(footnotes_in_view)
+
+	def calculate_section_count(self):
+		sections_in_view = self.view.find_all("<section id=\"section-[0-9]+\">", sublime.FindFlags.WRAP)
+		self.number_of_sections_in_file = len(sections_in_view)
 
 	def footnote_completion(self):
 		new_snippet = self.footnote_snippet.format(f"{self.number_of_footnotes_in_file + 1}")
@@ -41,6 +55,16 @@ class DynamicSnippetsViewEventListener(sublime_plugin.ViewEventListener):
 			kind=sublime.KIND_SNIPPET
 		)
 
+	def new_section_completion(self):
+		new_snippet = self.new_section_snippet.format(f"{self.number_of_sections_in_file + 1}")
+		return sublime.CompletionItem(
+			"blag_section",
+			annotation = "a new section",
+			completion = new_snippet,
+			completion_format = sublime.COMPLETION_FORMAT_SNIPPET,
+			kind=sublime.KIND_SNIPPET
+		)		
+
 	def on_query_completions(self, prefix, locations):
 		for point in locations:
 			in_html_scope = self.view.match_selector(point, "text.html.basic")
@@ -51,9 +75,11 @@ class DynamicSnippetsViewEventListener(sublime_plugin.ViewEventListener):
 			return None
 
 		self.calculate_footnote_count()
+		self.calculate_section_count()
 
 		prefilled_snippets = [
 			self.footnote_completion(),
-			self.footnote_ref_completion()
+			self.footnote_ref_completion(),
+			self.new_section_completion()
 		]
 		return sublime.CompletionList(prefilled_snippets)
